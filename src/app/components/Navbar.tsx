7dafcc49c3fe6a6510174061f1b3b59d011426da
arkhanardana <hanzmantap@gmail.com>
2024-09-30 20:19:47 +0700

    minify code navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,12 +7,14 @@ export default function Navbar() {
 	return (
 		<nav className="sticky p-3 w-full bg-white mb-9 top-0">
 			<ul className="flex justify-between items-center mx-4 sm:mx-14">
-				<Link href="/" className="text-xl text-[#070f2b] font-semibold cursor-pointer">
-					AnimeList
-				</Link>
-				<Link href="https://github.com/arkhanardana">
-					<GithubLogo size={40} className="cursor-pointer" />
-				</Link>
+				<li>
+					<Link href="/" className="text-xl text-[#070f2b] font-semibold cursor-pointer">
+						AnimeList
+					</Link>
+					<Link href="https://github.com/arkhanardana">
+						<GithubLogo size={40} className="cursor-pointer" />
+					</Link>
+				</li>
 			</ul>
 		</nav>
 	);
