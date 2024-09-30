ad2c04578730a5e3e62788638d69f52461ec9737
arkhanardana <hanzmantap@gmail.com>
2024-09-30 20:23:02 +0700

    fix bug navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import { GithubLogo } from "@phosphor-icons/react";
 import Link from "next/link";
 
 export default function Navbar() {
 	return (
 		<nav className="sticky p-3 w-full bg-white mb-9 top-0">
-			<ul className="flex justify-between items-center mx-4 sm:mx-14">
-				<li>
+			<ul>
+				<li className="flex justify-between items-center mx-4 sm:mx-14">
 					<Link href="/" className="text-xl text-[#070f2b] font-semibold cursor-pointer">
 						AnimeList
 					</Link>
